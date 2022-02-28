c14c38397aaff9e4585af250867f0d4043884bc0
Sharimiqbal <94395537+Sharimiqbal@users.noreply.github.com>
2022-02-28 18:32:43 +0530

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -23,10 +23,12 @@ import os
 from pynput.keyboard import Key, Controller
 from mutagen.mp3 import MP3
 from ToTime import toTime
+import random
 
     
 
 def songs(e=None):
+    #pylint: disable=unused-argument"
     global song_list
     song_list = os.listdir("tk_song")
 
@@ -36,7 +38,7 @@ def main():
     global currentSong
 
     def start_app(e=None):
-        
+        #pylint: disable=unused-argument"
         songs()
         global data, player, label1, label2, full_song_time,label4
         player = pyglet.media.Player()
@@ -68,7 +70,7 @@ def main():
             song = f"tk_song/{song_list[data]}"
             src = pyglet.media.load(song)
 
-        except Exception as e:
+        except:
             song = f"tk_song/{song_list[data-1]}"
             src = pyglet.media.load(song)
 
@@ -82,7 +84,7 @@ def main():
             label_text = (
                 ((song_list[len(song_list)-1]).replace('_', " ").replace('-', ' ').replace(".mp3", ""))).title()
 
-        label1 = Label(root, text='', bg="Light Blue", font=('bold', 20, "normal"))
+        label1 = Label(root, text='',bg="Light Blue", font=('bold', 20, "normal"))
 
         label1.grid(row=0, column=1, columnspan=4, pady=20)
         label1.config(text=label_text)
@@ -99,42 +101,52 @@ def main():
     songs()
 
     def play_song(e=None):
+        #pylint: disable=unused-argument"
         """Play The Song"""
         def time_function(e=None):
+            #pylint: disable=unused-argument"
             """Track The Time And Add Subtitle To Song"""
             global timer_
             label2.config(text=f'{toTime(round(player.time))}    of')
-            if player.time >= full_song_time:
-                nex()
+            if loop_var.get() == 0:
+                if player.time >= full_song_time:
+                    nex()
+            else:
+                if player.time >= full_song_time:
+                    nex()
+                    prev()
+                
             try:
                 with open(f"lyrics\\{label1.cget('text')}.txt",encoding="utf-8")as file:
                     
                     lyrics = eval(file.read()) # --:> A Dictionary
-                subtitles_check.config(state='normal')
+                subtitles_check.config(state='normal',cursor='hand2')
                 try:
                     label4.config(text = f"♫{(lyrics[label2.cget('text')[:5]])}♫")
                 except:
                     pass
             except Exception as e:
                 label4.config(text='Subtitles Not Available.')
-                subtitles_check.config(state='disabled')
+                subtitles_check.config(state='disabled',cursor='arrow')
                 
             timer_ = root.after(1000, time_function)
         time_function()
-        button_2.focus_set()
+        pause_btn.focus_set()
         player.play()
-        button_1.grid_forget()
-        button_2.grid(row=1, column=2, padx=20)
+        play_btn.grid_forget()
+        pause_btn.grid(row=1, column=2, padx=20)
 
     def pause_song(e=None):
+        #pylint: disable=unused-argument"
         """Pause The Song"""
         root.after_cancel(timer_)
         player.pause()
-        button_2.grid_forget()
-        button_1.focus_set()
-        button_1.grid(row=1, column=2, padx=20)
+        pause_btn.grid_forget()
+        play_btn.focus_set()
+        play_btn.grid(row=1, column=2, padx=20)
 
     def combo_entry_updater(e=None):
+        #pylint: disable=unused-argument"
         """Refresh The ComboBox Entries"""
         try:
             currentSong.current(data)
@@ -142,6 +154,7 @@ def main():
             currentSong.current(len(song_list)-1)
 
     def prev(e=None):
+        #pylint: disable=unused-argument"
         """Go To The Previous Song On The List"""
         label4.config(text="")
         label1.grid_forget()
@@ -160,6 +173,7 @@ def main():
         combo_entry_updater()
 
     def nex(event=None):
+        #pylint: disable=unused-argument"
         """Go To The Next Song On The List"""
         label4.config(text="")
         label1.grid_forget()
@@ -171,67 +185,32 @@ def main():
         else:
             with open("current_song.txt", "w") as f:
                 f.write(str(0))
+   
 
         pause_song()
         start_app()
         play_song()
         combo_entry_updater()
-
-    root = Tk()
-    root.config(
-        padx=100,
-        pady=100,
-        bg="Light Blue",
-        width=400,
-        height=350
-    )
-
-    root.maxsize(width=400, height=400)
-    root.minsize(width=400, height=400)
-    start_app()
-
-    btn1 = PhotoImage(master=root, file="tk_photo/play.png")
-    button_1 = Button(root, image=btn1, text="Play",
-                    command=play_song, bg='Light Blue', borderwidth=0, activebackground="Light Blue", cursor="hand2")
-
-    button_1.grid(row=1, column=2, padx=20)
-    button_1.focus_set()
-
-    btn2 = PhotoImage(master=root, file="tk_photo/pause.png")
-    button_2 = Button(root, image=btn2, bg="Light Blue",
-                    text="Pause", command=pause_song, borderwidth=0, activebackground="Light Blue", cursor="hand2")
-
-    btn3 = PhotoImage(master=root, file="tk_photo/next.png")
-    button_3 = Button(root, image=btn3, bg="Light Blue",
-                    text="next", command=nex, borderwidth=0, activebackground="Light Blue", cursor="hand2")
-
-    button_3.grid(row=1, column=4, padx=15)
-
-    btn4 = PhotoImage(master=root, file="tk_photo/previous.png")
-    button_4 = Button(root, image=btn4, command=prev,
-                    bg="Light Blue", borderwidth=0, activebackground="Light Blue", cursor="hand2")
-    button_4.grid(row=1, column=1, padx=20,)
-
-    n = StringVar()
-    currentSong = ttk.Combobox(root, width=18,
-                    textvariable=n, state="readonly", height=6)
-
-
-    i = 1
-    song_name = [
-        f"{i+song_list.index(value)}. {value.replace('.mp3', '').replace('-', ' ').replace('_', ' ').title()}" for value in song_list]
-    currentSong['values'] = song_name
-
-    currentSong.grid(column=1, row=2, columnspan=4,pady=10)
-
-    keyboard = Controller()
-
+    def random_song(e=None):
+        #pylint: disable=unused-argument"
+        label1.config(text="")
+        label4.config(text="")
+        with open('current_song.txt','w')as f:
+            f.write(str(random.randint(0,len(song_list)-1)))
+        pause_song()
+        start_app()
+        play_song()
+        combo_entry_updater()
+    
+    
     def v_up(e=None):
+        #pylint: disable=unused-argument"
         """Increase The Volume"""
         keyboard.press(Key.media_volume_up)
         keyboard.release(Key.media_volume_up)
 
     def v_down(e=None):
+        #pylint: disable=unused-argument"
         """Decrease The Volume"""
         keyboard.press(Key.media_volume_down)
         keyboard.release(Key.media_volume_down)
@@ -240,28 +219,14 @@ def main():
             label4.grid(column=0,row=6,columnspan=10)
         else:
             label4.grid_forget()
-
-    downImage = PhotoImage(file="tk_photo/down.png")
-    volume_down = Button(image=downImage, bg="Light Blue", cursor="hand2", command=v_down,
-                         borderwidth=0, activebackground="Light Blue")
-
-    volume_down.grid(column=0, row=1, columnspan=1)
-
-    upImage = PhotoImage(master=root, file="tk_photo/up.png")
-    volume_up = Button(image=upImage, bg="Light Blue", cursor="hand2",
-                       command=v_up, borderwidth=0, activebackground="Light Blue")
-    volume_up.grid(column=5, row=1, columnspan=1)
-    var_subtitles = IntVar(value=1)
-    subtitles_check = Checkbutton(root, text='lyrics',variable=var_subtitles, onvalue=1,command=subtitle, offvalue=0,bg="Light Blue",activebackground="Light Blue")
-    subtitles_check.grid(column=3, row=5, columnspan=2,pady=10)
+            
     
-    combo_entry_updater()
-    
+     
     
     def combo(event=None):
+        #pylint: disable=unused-argument"
         """Change Song Using ComboBox Entries"""
         label4.config(text="")
-        length = len(song_list)
         with open("current_song.txt")as file:
             file_data = file.read()
 
@@ -276,27 +241,100 @@ def main():
             start_app()
             play_song()
 
-    currentSong.bind("<<ComboboxSelected>>", combo)
 
     def focusSet(e=None):
+        #pylint: disable=unused-argument"
         """Focus On The Play And Pause Song"""
         songs()
         i = 1
         song_name = [
         f"{i+song_list.index(value)}. {value.replace('.mp3', '').replace('-', ' ').replace('_', ' ').title()}" for value in song_list]
         currentSong['values'] = song_name
-        button_1.focus_set()
-        button_2.focus_set()
+        play_btn.focus_set()
+        pause_btn.focus_set()
     
     def double_click(e=None):
+        #pylint: disable=unused-argument"
         """Play or Pause The Song On Mouse Double Click"""
         if player.playing:
             pause_song()
         else:
             play_song()
         
+    root = Tk()
+    root.config(
+        padx=100,
+        pady=100,
+        bg="Light Blue",
+        width=400,
+        height=350
+    )
+
+    root.maxsize(width=450, height=400)
+    root.minsize(width=450, height=400)
+    start_app()
+
+    play_img = PhotoImage(master=root, file="tk_photo/play.png")
+    play_btn = Button(root, image=play_img, text="Play",
+                    command=play_song, bg='Light Blue', borderwidth=0, activebackground="Light Blue", cursor="hand2")
+
+    play_btn.grid(row=1, column=2, padx=20)
+    play_btn.focus_set()
+
+    pause_img = PhotoImage(master=root, file="tk_photo/pause.png")
+    pause_btn = Button(root, image=pause_img, bg="Light Blue",
+                    text="Pause", command=pause_song, borderwidth=0, activebackground="Light Blue", cursor="hand2")
+
+    nex_img = PhotoImage(master=root, file="tk_photo/next.png")
+    nex_btn = Button(root, image=nex_img, bg="Light Blue",
+                    text="next", command=nex, borderwidth=0, activebackground="Light Blue", cursor="hand2")
+
+    nex_btn.grid(row=1, column=4, padx=15)
+
+    prev_img = PhotoImage(master=root, file="tk_photo/previous.png")
+    prev_btn = Button(root, image=prev_img, command=prev,
+                    bg="Light Blue", borderwidth=0, activebackground="Light Blue", cursor="hand2")
+    prev_btn.grid(row=1, column=1, padx=20,)
+
+    currentSong_var = StringVar()
+    currentSong = ttk.Combobox(root, width=18,
+                    textvariable=currentSong_var, state="readonly", height=6)
+
+
+    i = 1
+    song_name = [
+        f"{i+song_list.index(value)}. {value.replace('.mp3', '').replace('-', ' ').replace('_', ' ').title()}" for value in song_list]
+    currentSong['values'] = song_name
+
+    currentSong.grid(column=1, row=2, columnspan=4,pady=10)
+
+    currentSong.bind("<<ComboboxSelected>>", combo)
+
+
+    keyboard = Controller()
+    shuffle_btn = Button(root,text='Shuffle',cursor="hand2",command=random_song,font=("arial",7,"normal"),bg="Light Blue",activebackground="Light Blue",borderwidth=0)
+    shuffle_btn.grid(column=0,row=2)
+    loop_var = IntVar(value=0)
+    loop_btn = Checkbutton(root, text='Loop',cursor="hand2",variable=loop_var, onvalue=1, offvalue=0,bg="Light Blue",activebackground="Light Blue")
+    loop_btn.grid(column=5,row=2)
+    downImage = PhotoImage(file="tk_photo/down.png")
+    volume_down_btn = Button(image=downImage, bg="Light Blue", cursor="hand2", command=v_down,
+                         borderwidth=0, activebackground="Light Blue")
+
+    volume_down_btn.grid(column=0, row=1, columnspan=1)
+
+    upImage = PhotoImage(master=root, file="tk_photo/up.png")
+    volume_up_btn = Button(image=upImage, bg="Light Blue", cursor="hand2",
+                       command=v_up, borderwidth=0, activebackground="Light Blue")
+    volume_up_btn.grid(column=5, row=1, columnspan=1)
+    var_subtitles = IntVar(value=1)
+    subtitles_check = Checkbutton(root, text='lyrics',cursor="hand2",variable=var_subtitles, onvalue=1,command=subtitle, offvalue=0,bg="Light Blue",activebackground="Light Blue")
+    subtitles_check.grid(column=3, row=5, columnspan=2,pady=10)
+    
+    combo_entry_updater()
+   
     root.bind('<Button-1>', focusSet)
-#     root.bind('<Double-Button-1>', double_click)
+#     root.bind('<Double-Button-1>', double_click) #--- paly or pause song on double click
 #     root.bind('<Left>', prev)
 #     root.bind('<Right>', nex)
     root.bind('<Up>', v_up)
